392fcc916afdac3530da3fd57ef203e646eff717
QasimVibes <qasimrazzaq921@gmail.com>
2024-10-02 16:48:51 +0500

    feat: Set up initial project structure

---
+++ src/constants/Colors.ts
@@ -0,0 +1,14 @@
+export const COLORS = {
+  primary: '#F5CB58',
+  secondary: '#F3E9B5',
+  success: '#28a745',
+  info: '#17a2b8',
+  warning: '#ffc107',
+  danger: '#dc3545',
+  light: '#F5F5F5',
+  lightWhite: '#F8F8F8',
+  dark: '#391713',
+  lightDark: '#252525',
+  orangeBase: '#E95322',
+  orangeLight: '#FFDECF',
+};
---
+++ src/constants/Images.ts
@@ -0,0 +1,7 @@
+import GOBACK from '../assets/images/goBack.png';
+import GMAIL from '../assets/images/gmail.png';
+import FACEBOOK from '../assets/images/facebook.png';
+import USER from '../assets/images/user.png';
+import HOME from '../assets/images/home.png';
+
+export {GOBACK, GMAIL, FACEBOOK, USER, HOME};
